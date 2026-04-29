7cbe1c59200b14844b6de854b2dabec38d37e1d6
ROOC Indo Helper <andreas7290@gmail.com>
2026-04-29 16:32:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 const express = require('express');
 
-// This creates a tiny web page so Render keeps your bot alive
 const app = express();
 app.get('/', (req, res) => res.send('My bot is awake!'));
 app.listen(process.env.PORT |
 
 | 3000, () => console.log('Web server started'));
 
-// This creates your Discord bot with the correct intents
-const client = new Client({ 
+const client = new Client({
   intents:
 });
 
@@ -17,5 +15,4 @@ client.once('ready', () => {
     console.log(`Success! Logged in as ${client.user.tag}`);
 });
 
-// This logs your bot into Discord
 client.login(process.env.DISCORD_TOKEN);
