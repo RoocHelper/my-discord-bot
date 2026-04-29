0fc4a4f64053939768e4ff7abb2ac0bcdbfa2438
ROOC Indo Helper <andreas7290@gmail.com>
2026-04-29 16:41:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 const express = require('express');
 
-// 1. Create a tiny web page so Render keeps your bot alive
 const app = express();
 app.get('/', (req, res) => res.send('My bot is awake!'));
 
-// We use an "if" statement here to completely avoid the symbol that was causing the crash
 let port = process.env.PORT;
 if (!port) {
     port = 3000;
 }
 app.listen(port, () => console.log('Web server started'));
 
-// 2. Create your Discord bot with the correct intents
-const client = new Client({ 
+const client = new Client({
     intents:
 });
 
@@ -21,5 +18,4 @@ client.once('ready', () => {
     console.log(`Success! Logged in as ${client.user.tag}`);
 });
 
-// 3. Log your bot into Discord
 client.login(process.env.DISCORD_TOKEN);
